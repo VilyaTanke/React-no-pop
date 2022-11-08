143735e914aeeedd7abd6e96dc7e43b57bf8609d
Angel Magallanes <angelmagallanes80@gmail.com>
2022-11-08 08:14:43 +0100

    se configura el cliente para sincronizar nodepop-api

--- src/api/client.js
+++ src/api/client.js
@@ -3,3 +3,22 @@ import axios from 'axios';
 const client = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL,
 });
+
+client.interceptors.response.use(
+    response => response.data,
+    error => {
+        if (!error.response) {
+            return Promise.rejecty({ message: error.message });
+        }
+        return Promise.reject({
+            message: error.response.statusText,
+            ...error.response,
+            ...error.response.data,
+        });
+    },
+);
+
+export const setTokenAuthority = accessToken =>
+    (client.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`);
+
+export default client;
